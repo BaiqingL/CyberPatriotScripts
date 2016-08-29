d8c020c189521a706ea04164f6277448dad3e3e0
VBQL <baiqinglyu@gmail.com>
2016-08-29 13:36:32 -0400

    Update PasswdPolicy.bat

--- Windows/PasswdPolicy.bat
+++ Windows/PasswdPolicy.bat
@@ -1,1 +1,14 @@
+@echo off
+echo Check for Admn
+net sessions
+if %errorlevel%==0 (
+echo Success, Obtained Admin Rights
+) else (
+echo Failure, No Admin Rights
+pause
+exit
+)
 net accounts /minpwlen:10
+net accounts /maxpwage:30
+net accounts /minpwage:1
+net accounts
