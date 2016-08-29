70e826168500a9d78975dfca539012996c193d89
VBQL <baiqinglyu@gmail.com>
2016-08-29 01:08:57 -0400

    Update windows.bat

--- Windows/windows.bat
+++ Windows/windows.bat
@@ -2,3 +2,4 @@ REM Add other things if could be automated, RUN THIS AS ADMIN
 @echo off
 REM Windows Defender scan 
 cd C:\Program Files\Windows Defender\MpCmdRun.exe -Scan 2
+REM BitDefenser scanner if possible
