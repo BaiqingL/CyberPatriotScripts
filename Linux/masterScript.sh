20ee0c315ed60cf78989c3c5c86e287510af92a2
VBQL <baiqinglyu@gmail.com>
2016-08-29 14:38:39 -0400

    Update masterScript.sh

--- Linux/masterScript.sh
+++ Linux/masterScript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #MIT Licence 
 #Copyright (c) Ethan Perry, Andy Lyu
-#Welcome to CyberPatriot. I'll be your guide.
+echo Welcome to CyberPatriot. I'll be your guide.
 
 if [whoami != root]; then 
 	echo "Please run as root"
