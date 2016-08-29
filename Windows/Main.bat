3cc8ae70ddc371b7a7991d1a5e22e42ddbe4eae7
VBQL <baiqinglyu@gmail.com>
2016-08-29 17:31:19 -0400

    Update Main.bat

--- Windows/Main.bat
+++ Windows/Main.bat
@@ -25,3 +25,11 @@ net start > StartedProcesses.txt
 if %errorlevel%==1 echo Started processes failed to write
 reg export HKLM\Software\Microsoft\Windows\CurrentVersion\Run  Run.reg
 if %errorlevel%==1 echo Run processes failed to write
+REM Passwords must be 10 digits
+net accounts /minpwlen:10
+REM Passwords must be changed every 30 days
+net accounts /maxpwage:30
+REM Passwords can only be changed after 1 day has passed
+net accounts /minpwage:1
+REM Display current password policy
+net accounts
