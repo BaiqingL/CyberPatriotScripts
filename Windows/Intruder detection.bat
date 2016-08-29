057077a3079f14f233472ba584144e178c1bdf62
VBQL <baiqinglyu@gmail.com>
2016-08-29 01:57:54 -0400

    Update and rename windows.bat to Intruder detection.bat

--- Windows/Intruder detection.bat
+++ Windows/Intruder detection.bat
@@ -1,7 +1,8 @@
 REM Add other things if could be automated, RUN THIS AS ADMIN
 @echo off
 REM Windows Defender scan 
-C:\Program Files\Windows Defender\MpCmdRun.exe -Scan 2
+cd C:\Program Files\Windows Defender\
+MpCmdRun.exe -Scan 2
 REM Listing possible penetrations
 wmic process list brief
 PAUSE
