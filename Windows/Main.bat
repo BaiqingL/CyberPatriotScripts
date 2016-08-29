48fe29361fa2ca283858521f140b275ba7cfee6a
VBQL <baiqinglyu@gmail.com>
2016-08-29 17:32:29 -0400

    Update Main.bat

--- Windows/Main.bat
+++ Windows/Main.bat
@@ -33,3 +33,67 @@ REM Passwords can only be changed after 1 day has passed
 net accounts /minpwage:1
 REM Display current password policy
 net accounts
+REM Turn on UAC
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
+REM No Remote Desktop
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+REM Windows auomatic updates
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 3 /f
+REM Removing good ol' insecure stuff
+dism /online /disable-feature /featurename:IIS-WebServerRole >NUL
+dism /online /disable-feature /featurename:IIS-WebServer >NUL
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures >NUL
+dism /online /disable-feature /featurename:IIS-HttpErrors >NUL
+dism /online /disable-feature /featurename:IIS-HttpRedirect >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45 >NUL
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics >NUL
+dism /online /disable-feature /featurename:IIS-HttpLogging >NUL
+dism /online /disable-feature /featurename:IIS-LoggingLibraries >NUL
+dism /online /disable-feature /featurename:IIS-RequestMonitor >NUL
+dism /online /disable-feature /featurename:IIS-HttpTracing >NUL
+dism /online /disable-feature /featurename:IIS-Security >NUL
+dism /online /disable-feature /featurename:IIS-URLAuthorization >NUL
+dism /online /disable-feature /featurename:IIS-RequestFiltering >NUL
+dism /online /disable-feature /featurename:IIS-IPSecurity >NUL
+dism /online /disable-feature /featurename:IIS-Performance >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic >NUL
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools >NUL
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools >NUL
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility >NUL
+dism /online /disable-feature /featurename:IIS-Metabase >NUL
+dism /online /disable-feature /featurename:IIS-HostableWebCore >NUL
+dism /online /disable-feature /featurename:IIS-StaticContent >NUL
+dism /online /disable-feature /featurename:IIS-DefaultDocument >NUL
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing >NUL
+dism /online /disable-feature /featurename:IIS-WebDAV >NUL
+dism /online /disable-feature /featurename:IIS-WebSockets >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationInit >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET45 >NUL
+dism /online /disable-feature /featurename:IIS-ASP >NUL
+dism /online /disable-feature /featurename:IIS-CGI >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIFilter >NUL
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes >NUL
+dism /online /disable-feature /featurename:IIS-CustomLogging >NUL
+dism /online /disable-feature /featurename:IIS-BasicAuthentication >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic >NUL
+dism /online /disable-feature /featurename:IIS-ManagementConsole >NUL
+dism /online /disable-feature /featurename:IIS-ManagementService >NUL
+dism /online /disable-feature /featurename:IIS-WMICompatibility >NUL
+dism /online /disable-feature /featurename:IIS-LegacyScripts >NUL
+dism /online /disable-feature /featurename:IIS-LegacySnapIn >NUL
+dism /online /disable-feature /featurename:IIS-FTPServer >NUL
+dism /online /disable-feature /featurename:IIS-FTPSvc >NUL
+dism /online /disable-feature /featurename:IIS-FTPExtensibility >NUL
+dism /online /disable-feature /featurename:TFTP >NUL
+dism /online /disable-feature /featurename:TelnetClient >NUL
+dism /online /disable-feature /featurename:TelnetServer >NUL
+REM START SCAN!
+Sfc.exe /scannow
+REM PowerShell!
+powershell Invoke-WebRequest -OutFile MBRTKit.exe https://data-cdn.mbamupdates.com/web/mbar-1.09.3.1001.exe
+MBRTKit.exe
