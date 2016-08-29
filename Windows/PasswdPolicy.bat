fcd5cc7dc36aef5a3b61f663049f2b87aed24e60
VBQL <baiqinglyu@gmail.com>
2016-08-29 13:37:51 -0400

    Update PasswdPolicy.bat

--- Windows/PasswdPolicy.bat
+++ Windows/PasswdPolicy.bat
@@ -8,7 +8,11 @@ echo Failure, No Admin Rights
 pause
 exit
 )
+REM Passwords must be 10 digits
 net accounts /minpwlen:10
+REM Passwords must be changed every 30 days
 net accounts /maxpwage:30
+REM Passwords can only be changed after 1 day has passed
 net accounts /minpwage:1
+REM Display current password policy
 net accounts
