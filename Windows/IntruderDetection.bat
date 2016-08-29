a9abd1fb3b74bd57dcb8ea3cb6d5b5fe46ffc50f
VBQL <baiqinglyu@gmail.com>
2016-08-29 02:38:05 -0400

    Update IntruderDetection.bat

--- Windows/IntruderDetection.bat
+++ Windows/IntruderDetection.bat
@@ -1,10 +1,18 @@
 REM Add other things if could be automated, RUN THIS AS ADMIN
 @echo off
+echo Check for Admn
+net sessions
+if %errorlevel%==0 (
+echo Success
+) else (
+echo No admin
+pause
+exit
+)
 REM Windows Defender scan 
 cd C:\Program Files\Windows Defender\
 MpCmdRun.exe -Scan 2
 REM Listing possible penetrations
-@echo on
 echo Staring to output active processes!
 wmic process list brief > BriefProcesses.txt
 if %errorlevel%==1 echo Brief Processes failed to write
