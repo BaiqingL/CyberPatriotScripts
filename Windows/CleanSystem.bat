3d7b845d2b9ce8c6099177c0744ebde02bd1d044
VBQL <baiqinglyu@gmail.com>
2016-08-29 02:25:55 -0400

    Create CleanSystem.bat

---
+++ Windows/CleanSystem.bat
@@ -0,0 +1,7 @@
+REM Turn on UAC
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
+REM No Remote Desktop
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+REM Windows auomatic updates
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 3 /f
