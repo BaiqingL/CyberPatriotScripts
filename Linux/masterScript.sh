3b36ff24db99b0dfc45cf04ebd9834af7301d3e1
Vuton <VutonQ@users.noreply.github.com>
2016-08-29 13:38:35 -0400

    Secure /etc/sysctl.conf

--- Linux/masterScript.sh
+++ Linux/masterScript.sh
@@ -88,6 +88,17 @@ ip6tables -P INPUT DROP
 ip6tables -P FORWARD DROP
 ip6tables -P OUTPUT DROP
 
+#--------- Secure /etc/sysctl.conf ----------------
+sysctl -w net.ipv4.tcp_syncookies=1
+sysctl -w net.ipv4.ip_forward=0
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.default.send_redirects=0
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.default.accept_redirects=0
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+sysctl -p
+
 #--------- Scan For Vulnerabilities and viruses ----------------
 chkrootkit -q
 rkhunter --update
