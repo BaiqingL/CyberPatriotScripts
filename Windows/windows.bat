3de70d508e841000bc4f94998a9d3e666988dde9
VBQL <baiqinglyu@gmail.com>
2016-08-29 01:11:21 -0400

    Update windows.bat

--- Windows/windows.bat
+++ Windows/windows.bat
@@ -1,7 +1,7 @@
 REM Add other things if could be automated, RUN THIS AS ADMIN
 @echo off
 REM Windows Defender scan 
-cd C:\Program Files\Windows Defender\MpCmdRun.exe -Scan 2
+C:\Program Files\Windows Defender\MpCmdRun.exe -Scan 2
 REM Listing possible penetrations
 wmic process list brief
 PAUSE
