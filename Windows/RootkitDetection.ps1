ffde62c1ce62175a6dd0b72b9eb36ecb777515e8
VBQL <baiqinglyu@gmail.com>
2016-08-29 13:50:32 -0400

    Update RootkitDetection.ps1

--- Windows/RootkitDetection.ps1
+++ Windows/RootkitDetection.ps1
@@ -1,4 +1,4 @@
-#Downloads rootkit detection
+#Downloads rootkit detection from MalwareBytes
 Invoke-WebRequest -OutFile MBRTKit.exe https://data-cdn.mbamupdates.com/web/mbar-1.09.3.1001.exe
-#Scan starts
+#Executes file
 .\MBRTKit.exe
