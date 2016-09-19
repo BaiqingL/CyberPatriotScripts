2d8a33e492c45d5c503f3005cfbc8bc8ae9668c9
Vuton <VutonQ@users.noreply.github.com>
2016-09-19 19:48:36 -0400

    Block Bogons

--- Linux/main.sh
+++ Linux/main.sh
@@ -131,6 +131,58 @@ ip6tables -P INPUT DROP
 ip6tables -P FORWARD DROP
 ip6tables -P OUTPUT DROP
 
+#Block Bogons
+printf "\033[1;31mEnter primary internet interface: \033[0m\n"
+read interface
+
+#Blocks bogons going into the computer
+iptables -A INPUT -s 127.0.0.0/8 -i $interface -j DROP
+iptables -A INPUT -s 0.0.0.0/8 -j DROP
+iptables -A INPUT -s 100.64.0.0/10 -j DROP
+iptables -A INPUT -s 169.254.0.0/16 -j DROP
+iptables -A INPUT -s 192.0.0.0/24 -j DROP
+iptables -A INPUT -s 192.0.2.0/24 -j DROP
+iptables -A INPUT -s 198.18.0.0/15 -j DROP
+iptables -A INPUT -s 198.51.100.0/24 -j DROP
+iptables -A INPUT -s 203.0.113.0/24 -j DROP
+iptables -A INPUT -s 224.0.0.0/3 -j DROP
+
+#Blocks bogons from leaving the computer
+iptables -A OUTPUT -d 127.0.0.0/8 -o $interface -j DROP
+iptables -A OUTPUT -d 0.0.0.0/8 -j DROP
+iptables -A OUTPUT -d 100.64.0.0/10 -j DROP
+iptables -A OUTPUT -d 169.254.0.0/16 -j DROP
+iptables -A OUTPUT -d 192.0.0.0/24 -j DROP
+iptables -A OUTPUT -d 192.0.2.0/24 -j DROP
+iptables -A OUTPUT -d 198.18.0.0/15 -j DROP
+iptables -A OUTPUT -d 198.51.100.0/24 -j DROP
+iptables -A OUTPUT -d 203.0.113.0/24 -j DROP
+iptables -A OUTPUT -d 224.0.0.0/3 -j DROP
+
+#Blocks outbound from source bogons - A bit overkill
+iptables -A OUTPUT -s 127.0.0.0/8 -o $interface -j DROP
+iptables -A OUTPUT -s 0.0.0.0/8 -j DROP
+iptables -A OUTPUT -s 100.64.0.0/10 -j DROP
+iptables -A OUTPUT -s 169.254.0.0/16 -j DROP
+iptables -A OUTPUT -s 192.0.0.0/24 -j DROP
+iptables -A OUTPUT -s 192.0.2.0/24 -j DROP
+iptables -A OUTPUT -s 198.18.0.0/15 -j DROP
+iptables -A OUTPUT -s 198.51.100.0/24 -j DROP
+iptables -A OUTPUT -s 203.0.113.0/24 -j DROP
+iptables -A OUTPUT -s 224.0.0.0/3 -j DROP
+
+#Block receiving bogons intended for bogons - Super overkill
+iptables -A INPUT -d 127.0.0.0/8 -i $interface -j DROP
+iptables -A INPUT -d 0.0.0.0/8 -j DROP
+iptables -A INPUT -d 100.64.0.0/10 -j DROP
+iptables -A INPUT -d 169.254.0.0/16 -j DROP
+iptables -A INPUT -d 192.0.0.0/24 -j DROP
+iptables -A INPUT -d 192.0.2.0/24 -j DROP
+iptables -A INPUT -d 198.18.0.0/15 -j DROP
+iptables -A INPUT -d 198.51.100.0/24 -j DROP
+iptables -A INPUT -d 203.0.113.0/24 -j DROP
+iptables -A INPUT -d 224.0.0.0/3 -j DROP
+
 iptables -A INPUT -i lo -j ACCEPT
 
 #Least Strict Rules
