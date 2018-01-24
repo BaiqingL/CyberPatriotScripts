824ab3f40cb8f007d9a2e5b9abbdbf0f7a0c4833
AndyL <baiqinglyu@gmail.com>
2018-01-24 13:00:30 -0500

    Update FirstRun.bat

--- Windows/FirstRun.bat
+++ Windows/FirstRun.bat
@@ -235,8 +235,8 @@ REM Passwords must be 10 digits
 net accounts /minpwlen:10
 REM Passwords must be changed every 30 days
 net accounts /maxpwage:30
-REM Passwords can only be changed after 1 day has passed
-net accounts /minpwage:1
+REM Passwords can only be changed after 5 day has passed
+net accounts /minpwage:5
 REM Display current password policy
 echo "CURRENT POLICY"
 PAUSE
